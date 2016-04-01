d9adec0756229dacd4c913eb3e3c1d2d4c60a303
Teodora Garasanin <teodora.garashanin@gmail.com>
2016-04-01 15:55:33 +0200

    LogIn view controller almost completed. Placeholder. Color.

--- ToDo/ToDo/LogInViewController.m
+++ ToDo/ToDo/LogInViewController.m
@@ -8,5 +8,49 @@
 
 #import "LogInViewController.h"
 
+@interface LogInViewController()
+@property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@end
+
 @implementation LogInViewController
+
+#pragma mark - Private API
+
+-(void)configureTextField:(UITextField *) textField{
+    
+   if (textField.placeholder.length>0){
+//        UIColor *color = [UIColor colorWithRed:117.0f/255.0f
+//                                         green:113.0f/255.0f
+//                                          blue:111.0f/255.0f
+//                                         alpha:1.0f];
+    
+       NSDictionary *attributes=@{NSForegroundColorAttributeName: [UIColor whiteColor],
+                               NSFontAttributeName:[UIFont fontWithName:@"AvenirNext-Regular" size:14.0]};
+    
+    textField.attributedPlaceholder=[[NSAttributedString alloc]initWithString:textField.placeholder
+                                                                  attributes:attributes];
+}
+}
+
+#pragma mark -View lifecycle
+-(void) viewDidLoad{
+    [super viewDidLoad];
+    [self configureTextField:self.usernameTextField];
+    [self configureTextField:self.passwordTextField];
+}
+
+#pragma mark - Actions
+
+- (IBAction)signInButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)forgotPasswordButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)signUpButtonTapped:(UIButton *)sender {
+}
+
 @end
