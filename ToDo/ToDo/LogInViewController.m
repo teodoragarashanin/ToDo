94962e59d2d821952ec0d42d36e58b6c79c615b0
Teodora Garasanin <teodora.garashanin@gmail.com>
2016-04-04 16:00:21 +0200

    Delegate.

--- ToDo/ToDo/LogInViewController.m
+++ ToDo/ToDo/LogInViewController.m
@@ -1,56 +1,109 @@
 //
-//  LogInViewController.m
+//  LoginViewController.m
 //  ToDo
 //
-//  Created by Cubes School 5 on 3/30/16.
-//  Copyright © 2016 Cubes School 5. All rights reserved.
+//  Created by Djuro Alfirevic on 3/30/16.
+//  Copyright © 2016 Djuro Alfirevic. All rights reserved.
 //
 
-#import "LogInViewController.h"
+#import "LoginViewController.h"
 
-@interface LogInViewController()
+#define kConstant 50.0
+
+@interface LoginViewController() <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UIView *logoView;
 @end
 
-@implementation LogInViewController
+@implementation LoginViewController
 
 #pragma mark - Private API
 
--(void)configureTextField:(UITextField *) textField{
-    
-   if (textField.placeholder.length>0){
-//        UIColor *color = [UIColor colorWithRed:117.0f/255.0f
-//                                         green:113.0f/255.0f
-//                                          blue:111.0f/255.0f
-//                                         alpha:1.0f];
-    
-       NSDictionary *attributes=@{NSForegroundColorAttributeName: [UIColor whiteColor],
-                               NSFontAttributeName:[UIFont fontWithName:@"AvenirNext-Regular" size:14.0]};
-    
-    textField.attributedPlaceholder=[[NSAttributedString alloc]initWithString:textField.placeholder
-                                                                  attributes:attributes];
-}
-}
-
-#pragma mark -View lifecycle
--(void) viewDidLoad{
-    [super viewDidLoad];
-    [self configureTextField:self.usernameTextField];
-    [self configureTextField:self.passwordTextField];
+- (void)configureTextField:(UITextField *)textField {
+    if (textField.placeholder.length > 0) {
+//        UIColor *color = [UIColor colorWithRed:117.0/255.0
+//                                         green:113.0/255.0
+//                                          blue:111.0/255.0
+//                                         alpha:1.0];
+        
+        NSDictionary *attributes = @{
+                                     NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Regular" size:14.0],
+                                     NSForegroundColorAttributeName: [UIColor whiteColor]
+                                     };
+        
+        textField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:textField.placeholder
+                                                                          attributes:attributes];
+    }
 }
 
 #pragma mark - Actions
 
-- (IBAction)signInButtonTapped:(UIButton *)sender {
-}
-
 - (IBAction)forgotPasswordButtonTapped:(UIButton *)sender {
 }
 
+- (IBAction)signInButtonTapped:(UIButton *)sender {
+}
+
 - (IBAction)signUpButtonTapped:(UIButton *)sender {
+    NSLog(@"Sign up...");
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self configureTextField:self.usernameTextField];
+    [self configureTextField:self.passwordTextField];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [UIView animateWithDuration:3.0 animations:^{
+        self.logoView.alpha = 0.0;
+    }];
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [UIView animateWithDuration:10.0
+                          delay:0.0
+         usingSpringWithDamping:0.4
+          initialSpringVelocity:10.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         CGRect frame = self.containerView.frame;
+                         frame.origin.y = frame.origin.y - kConstant;
+                         self.containerView.frame = frame;
+                     }
+                     completion:nil];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+         usingSpringWithDamping:0.4
+          initialSpringVelocity:10.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         CGRect frame = self.containerView.frame;
+                         frame.origin.y = frame.origin.y + kConstant;
+                         self.containerView.frame = frame;
+                     }
+                     completion:nil];
 }
 
 @end
--- ToDo/ToDo/ViewController.m
+++ ToDo/ToDo/ViewController.m
@@ -2,11 +2,154 @@
 //  ViewController.m
 //  ToDo
 //
-//  Created by Cubes School 5 on 3/28/16.
-//  Copyright © 2016 Cubes School 5. All rights reserved.
+//  Created by Djuro Alfirevic on 3/28/16.
+//  Copyright © 2016 Djuro Alfirevic. All rights reserved.
 //
 
 #import "ViewController.h"
 
+@interface ViewController() <UITextFieldDelegate>
+@property (strong, nonatomic) UIImageView *backgroundImageView;
+@property (strong, nonatomic) UIImageView *usernameImageView;
+@property (strong, nonatomic) UIImageView *passwordImageView;
+@property (strong, nonatomic) UITextField *usernameTextField;
+@property (strong, nonatomic) UITextField *passwordTextField;
+@property (strong, nonatomic) UIButton *forgotPasswordButton;
+@property (strong, nonatomic) UIButton *signInButton;
+@property (strong, nonatomic) UIButton *signUpButton;
+@property (strong, nonatomic) UILabel *dontHaveAccountLabel;
+@end
+
 @implementation ViewController
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Properties
+    UIFont *font = [UIFont fontWithName:@"AvenirNext-Regular" size:14.0];
+    NSDictionary *attribues = @{
+                                NSForegroundColorAttributeName: [UIColor whiteColor],
+                                NSFontAttributeName: font
+                                };
+    
+    // 1 - Background image
+    self.backgroundImageView = [[UIImageView alloc] init];
+    self.backgroundImageView.frame = CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height);
+    self.backgroundImageView.image = [UIImage imageNamed:@"login-bg"];
+    [self.view addSubview:self.backgroundImageView];
+    
+    // 2 - Username
+    self.usernameImageView = [[UIImageView alloc] init];
+    self.usernameImageView.frame = CGRectMake(30.0, 379.0, 19.0, 19.0);
+    self.usernameImageView.image = [UIImage imageNamed:@"username"];
+    [self.view addSubview:self.usernameImageView];
+    
+    // 3 - Password
+    self.passwordImageView = [[UIImageView alloc] init];
+    self.passwordImageView.frame = CGRectMake(30.0, 424.0, 19.0, 19.0);
+    self.passwordImageView.image = [UIImage imageNamed:@"password"];
+    [self.view addSubview:self.passwordImageView];
+    
+    // 4 - Username text field
+    CGRect rect = CGRectMake(69.0, 374.0, 286.0, 30.0);
+    
+    self.usernameTextField = [[UITextField alloc] init];
+    self.usernameTextField.frame = rect;
+    self.usernameTextField.borderStyle = UITextBorderStyleNone;
+    self.usernameTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.usernameTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.usernameTextField.keyboardType = UIKeyboardTypeEmailAddress;
+    self.usernameTextField.textColor = [UIColor whiteColor];
+    self.usernameTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Username" attributes:attribues];
+    self.usernameTextField.delegate = self;
+    [self.view addSubview:self.usernameTextField];
+    
+    // 5 - Password text field
+    self.passwordTextField = [[UITextField alloc] init];
+    self.passwordTextField.frame = CGRectMake(rect.origin.x, 419.0, rect.size.width, rect.size.height);
+    self.passwordTextField.borderStyle = self.usernameTextField.borderStyle;
+    self.passwordTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.passwordTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.passwordTextField.secureTextEntry = YES;
+    self.passwordTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Password" attributes:attribues];
+    self.passwordTextField.textColor = [UIColor whiteColor];
+    self.passwordTextField.delegate = self;
+    [self.view addSubview:self.passwordTextField];
+    
+    // 6 - Separator (initWithFrame !!!)
+    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 408.0, self.view.frame.size.width, 1.0)];
+    separatorView.backgroundColor = [UIColor lightGrayColor];
+    separatorView.alpha = 0.6;
+    [self.view addSubview:separatorView];
+    
+    // 7 - Forgot password?
+    self.forgotPasswordButton = [[UIButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width-118.0-10.0, 464.0, 118.0, 30.0)];
+    [self.forgotPasswordButton setTitle:@"Forgot password?" forState:UIControlStateNormal];
+    [self.forgotPasswordButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [self.forgotPasswordButton setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    self.forgotPasswordButton.titleLabel.font = font;
+    [self.forgotPasswordButton addTarget:self action:@selector(forgotPasswordButtonTapped) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.forgotPasswordButton];
+    
+    // 8 - Sign in?
+    self.signInButton = [[UIButton alloc] initWithFrame:CGRectMake(0.0, self.view.frame.size.height-112.0, self.view.frame.size.width, 60.0)];
+    [self.signInButton setTitle:@"Sign in" forState:UIControlStateNormal];
+    [self.signInButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.signInButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+    self.signInButton.backgroundColor = [UIColor colorWithRed:246.0/255.0 green:0.0/255.0 blue:65.0/255.0 alpha:1.0];
+    self.signInButton.titleLabel.font = font;
+    [self.signInButton addTarget:self action:@selector(signInButtonTapped) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.signInButton];
+    
+    // 9 - Don't have an account
+    self.dontHaveAccountLabel = [[UILabel alloc] initWithFrame:CGRectMake(83.0, self.view.frame.size.height-38.0, 150.0, 20.0)];
+    self.dontHaveAccountLabel.textColor = [UIColor lightGrayColor];
+    self.dontHaveAccountLabel.text = @"Don't have an account?";
+    self.dontHaveAccountLabel.font = [UIFont fontWithName:@"AvenirNext-Regular" size:14.0];
+    [self.view addSubview:self.dontHaveAccountLabel];
+    
+    // 10 - Sign Up
+    self.signUpButton = [[UIButton alloc] initWithFrame:CGRectMake(237.0, self.view.frame.size.height-44.0, 51.0, 32.0)];
+    [self.signUpButton setTitle:@"Sign Up" forState:UIControlStateNormal];
+    [self.signUpButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [self.signUpButton setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    self.signUpButton.titleLabel.font = font;
+    [self.signUpButton addTarget:self action:@selector(signUpButtonTapped) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.signUpButton];
+    
+    
+    // 5 - weak vs. strong
+}
+
+#pragma mark - Actions
+
+- (void)forgotPasswordButtonTapped {
+    NSLog(@"forgotPasswordButtonTapped");
+    
+    
+}
+
+- (void)signInButtonTapped {
+    NSLog(@"signInButtonTapped");
+}
+
+- (void)signUpButtonTapped {
+    NSLog(@"signUpButtonTapped");
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+   
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    
+}
+
 @end
